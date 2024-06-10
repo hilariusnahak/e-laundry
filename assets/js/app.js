b99b35301c8e96daa5b4271c46929f42f6d947f9
hilariusnahak6@gmail.com <hilariusnahak6@gmail.com>
2024-06-11 00:35:40 +0700

    Fitur: add google maps API & layout html-css-js

--- assets/js/app.js
+++ assets/js/app.js
@@ -44,6 +44,36 @@ const shadowHeader = () =>
 }
 window.addEventListener( 'scroll', shadowHeader )
 
+/*=============== MAPS ===============*/
+// Initialize and add the map
+let map;
+
+async function initMap ()
+{
+  // The location of Uluru
+  const position = { lat: -7.785596273500772, lng: 110.35765018823207 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary( "maps" );
+  const { AdvancedMarkerElement } = await google.maps.importLibrary( "marker" );
+
+  // The map, centered at Uluru
+  map = new Map( document.getElementById( "map" ), {
+    zoom: 15,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  } );
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement( {
+    map: map,
+    position: position,
+    title: "VR Loundry",
+  } );
+}
+
+initMap();
+
 /*=============== SHOW SCROLL UP ===============*/
 const scrollUp = () =>
 {
